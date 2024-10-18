23f001b6405e4efd36f5c2a50b79c77a33df9dad
jakebark <jakebark@amazon.co.uk>
2024-10-18 17:53:53 +0100

    more_vars

--- codepipeline.tf
+++ codepipeline.tf
@@ -164,7 +164,7 @@ data "aws_iam_policy_document" "codepipeline-policy" {
     ]
 
     resources = [
-      "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.codecommit_repo}"
+      "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.repo}"
     ]
   }
 
