2630a0506ca16e666352922ea6a9d9a3e7a66b8b
jakebark <jakebark@amazon.co.uk>
2024-10-21 11:54:39 +0100

    can_connection

--- codepipeline.tf
+++ codepipeline.tf
@@ -174,7 +174,7 @@ data "aws_iam_policy_document" "codepipeline" {
     ]
 
     resources = [
-      try(var.connection, "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.repo}")
+      can(var.connection) ? var.connection : "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.repo}"
     ]
   }
 }
