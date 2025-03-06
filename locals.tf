392a801defe6e3bf8d43313afeb8a19ebf3a9cc1
jakebark <jakebark@amazon.co.uk>
2025-03-06 22:29:05 +0000

    rm tf_version from lint

--- locals.tf
+++ locals.tf
@@ -12,7 +12,6 @@ locals {
   }
 
   env_var = {
-    TF_VERSION      = var.terraform_version
     TFLINT_VERSION  = var.tflint_version
     SAST_REPORT_ARN = aws_codebuild_report_group.sast.arn
     CHECKOV_SKIPS   = join(",", "${var.checkov_skip}")
