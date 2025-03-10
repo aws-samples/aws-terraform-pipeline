803c862ee55e3aa3bc3ed2dfb1dc0232f7a2b997
jakebark <jakebark@amazon.co.uk>
2025-03-10 07:01:17 +0000

    added tag-nag

--- locals.tf
+++ locals.tf
@@ -7,6 +7,7 @@ locals {
     fmt      = "hashicorp/terraform:${var.terraform_version}"
     lint     = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     sast     = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    tags     = "jakebark/tag-nag:${var.tagnag_version}"
   }
 
   conditional_validation_stages = {
@@ -19,5 +20,6 @@ locals {
     CHECKOV_SKIPS   = join(",", "${var.checkov_skip}")
     TAGNAG_TAGS     = var.tags
     TF_VERSION      = var.terraform_version
+    TAGNAG_TAGS     = var.tags
   }
 }
