394fa8ea69ba43a359f628581ea372ea45570ef9
jakebark <jakebark@amazon.com>
2025-03-14 13:40:49 +0000

    conditional validation stages

--- codebuild.tf
+++ codebuild.tf
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT-0
 
 module "validation" {
-  for_each              = local.validation_stages
+  for_each              = var.tags == "" ? local.validation_stages : merge(local.validation_stages, local.conditional_validation_stages)
   source                = "./modules/codebuild"
   codebuild_name        = "${var.pipeline_name}-${each.key}"
   codebuild_role        = aws_iam_role.codebuild_validate.arn
