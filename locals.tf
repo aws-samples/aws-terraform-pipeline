74260759020265eccd02e4afb67c49b68db1da06
jakebark <jakebark@amazon.com>
2025-03-18 15:31:50 +0000

    sast

--- locals.tf
+++ locals.tf
@@ -8,7 +8,7 @@ locals {
     validate = "hashicorp/terraform:${var.terraform_version}"
     fmt      = "hashicorp/terraform:${var.terraform_version}"
     lint     = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-    sast     = "bridgecrew/checkov:${var.checkov_version}"
+    sast     = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
   }
 
   conditional_validation_stages = {
