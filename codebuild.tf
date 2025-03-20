8f343bb4a50848f6a97ef0d3dde8b23c294abc11
jakebark <jakebark@amazon.com>
2025-03-20 20:39:16 +0000

    codebuild cwl permissions

--- codebuild.tf
+++ codebuild.tf
@@ -111,8 +111,7 @@ data "aws_iam_policy_document" "codebuild" {
     ]
 
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${local.log_group}",
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${local.log_group}:*"
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"
     ]
   }
 
