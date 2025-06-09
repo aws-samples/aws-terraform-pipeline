04092f6bc0fbdf6743cf5c6f3e8556ff690eb83f
jakebark <jakebark@amazon.com>
2025-06-09 21:07:15 +0100

    codebuild outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,16 @@ output "pipeline" {
 output "pipeline_role" {
   value = aws_iam_role.codepipeline_role
 }
+
+output "codebuild_validate_role" {
+  value = aws_iam_role.codebuild_validate
+}
+
+output "codebuild_execution_role" {
+  value = aws_iam_role.codebuild_execution
+}
+
+output "cloudwatch_log_group" {
+  value = aws_cloudwatch_log_group.this
+}
+
