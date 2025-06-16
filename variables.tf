ba91006dbb3acfec93fd5ed53a432e7120c9397f
jakebark <jakebark@amazon.com>
2025-06-16 09:02:02 +0100

    update var type (again)

--- variables.tf
+++ variables.tf
@@ -113,10 +113,10 @@ variable "tflint_version" {
 }
 
 variable "vpc" {
-  type = map(object({
+  type = object({
     vpc_id             = string
     subnets            = list(string)
     security_group_ids = list(string)
-  }))
+  })
   default = null
 }
