70714951092d746446812c6ae1e54506f66f3329
jakebark <jakebark@amazon.com>
2025-08-13 06:48:58 +0100

    updated region deprecation

--- eventbridge.tf
+++ eventbridge.tf
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_event_rule" "this" {
       "CodeCommit Repository State Change"
     ],
     "resources" : [
-      "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.repo}"
+      "arn:aws:codecommit:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${var.repo}"
     ],
     "detail" : {
       "event" : [
